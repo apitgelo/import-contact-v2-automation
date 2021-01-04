9f45565ddc5e953c1e00cbd3bf1c513bab009697
Hafidz Prasetya <apitgelo@gmail.com>
2021-01-04 19:06:18 +0700

    feat: using limit 3000 per request

--- app.js
+++ app.js
@@ -6,30 +6,41 @@ var faker = require('faker');
 const total = process.argv[2];
 const baseUrl = process.argv[3];
 const token = process.argv[4];
-const importContacts = async (file, headers) => {
-  await fs.promises.appendFile(file, headers, function (err) {
-    if (err) return console.log(err);
-  })
-
-  for (let index = 0; index < total; index++) {
-    const contact = `${faker.name.firstName()},,${faker.name.lastName()}_importcsv,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n`;
-    await fs.promises.appendFile(file, contact, function (err) {
-      if (err)
-        return console.log(err);
-    })
+const importContacts = async (total, file, headers) => {
+  var array = [];
+  while (total > 0) {
+    array.push(Math.min(total, 3000));
+    total = total - 3000;
   }
 
-  await request.post(`${baseUrl}/v2/contacts/import`)
-    .set('Authorization', `Bearer ${token}`)
-    .set('Content-Type', 'multipart/form-data')
-    .attach('file', file).then((result) => {
-      // process the result here
+  var totalCreated = 0;
+  for (const chunkedTotal of array) {
+    await fs.promises.appendFile(file, headers, function (err) {
+      if (err) return console.log(err);
     })
-    .catch((err) => {
-      throw err;
-    });
 
-  await fs.promises.unlink(file);
+    for (let index = 0; index < chunkedTotal; index++) {
+      const contact = `${faker.name.firstName()},,${faker.name.lastName()}_importcsv,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n`;
+      await fs.promises.appendFile(file, contact, function (err) {
+        if (err)
+          return console.log(err);
+      })
+
+      totalCreated+=1;
+    }
+
+    await request.post(`${baseUrl}/v2/contacts/import`)
+      .set('Authorization', `Bearer ${token}`)
+      .set('Content-Type', 'multipart/form-data')
+      .attach('file', file).then((result) => {
+          console.log(`${totalCreated} contacts imported.`);
+      })
+      .catch((err) => {
+        throw err;
+      });
+
+    await fs.promises.unlink(file);
+  };
 };
 
 const file = 'contact-import.csv';
@@ -38,4 +49,4 @@ const headers = ',,,,,,,,,,,,,,,,,ADDRESS 1,,,,ADDRESS 2,,,,,COMPANY ADDRESS,,,,
   'Captured/\n' +
   'Connected",Buyer,Seller,Bought,Sold,Agent,KW Agent,Allied Resource,Talent,Referral Partner,Downline,Tags,Notes,Source,Other Source,Facebook,Twitter,Linkedin,Google+,Pinterest,Instagram,Houzz\n';
 
-importContacts(file, headers);
+importContacts(total, file, headers);
